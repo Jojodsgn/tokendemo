04c755823fd53eee8f1554de6bcc6e00b29529b2
jojodsgn <johannes.peltzer@me.com>
2025-04-01 07:25:28 +0200

    .

--- src/registerSDFunctions.js
+++ src/registerSDFunctions.js
@@ -1,6 +1,6 @@
-import StyleDictionary from 'style-dictionary';
-import { register } from '@tokens-studio/sd-transforms';
+import StyleDictionary from "style-dictionary";
+import { register } from "@tokens-studio/sd-transforms";
 
 // sd-transforms, 2nd parameter for options can be added
 // See docs: https://github.com/tokens-studio/sd-transforms
-register(StyleDictionary);
+register(StyleDictionary, { excludeParentKeys: true });
